395454a372ff96c48ead77dc97815f15727a3d43
Linguardium <61733861+Linguardium@users.noreply.github.com>
2024-11-23 21:35:00 -0600

    fix scale being applied from the wrong model

--- src/main/java/dev/syoritohatsuki/armorexample/mixin/client/ItemRendererMixin.java
+++ src/main/java/dev/syoritohatsuki/armorexample/mixin/client/ItemRendererMixin.java
@@ -1,11 +1,9 @@
 package dev.syoritohatsuki.armorexample.mixin.client;
 
 import dev.syoritohatsuki.armorexample.registry.ItemsRegistry;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ModelTransformationMode;
 import org.spongepowered.asm.mixin.Final;
@@ -17,22 +15,21 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import static dev.syoritohatsuki.armorexample.client.ArmorExampleModClient.TRIDENT_ID;
 
 @Mixin(ItemRenderer.class)
-public class ItemRendererMixin {
+public abstract class ItemRendererMixin {
     @Shadow @Final private BakedModelManager bakedModelManager;
 
-    @ModifyVariable(method="renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ModelTransformationMode;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;Z)V", at= @At(value = "HEAD"),argsOnly = true)
+    @Shadow private static boolean shouldUseInventoryModel(ModelTransformationMode transformationMode) { throw new AssertionError(); };
+
+    @ModifyVariable(method={
+            "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;Z)V",
+            "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ModelTransformationMode;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;Z)V"
+    }, at= @At(value = "HEAD"),argsOnly = true)
     private BakedModel isEndTridentForRender(
             BakedModel original,
-    		ItemStack stack,
-            ModelTransformationMode transformationMode,
-            MatrixStack matrices,
-            VertexConsumerProvider vertexConsumers,
-            int light,
-            int overlay,
-            BakedModel model,
-            boolean useInventoryModel
+            ItemStack stack,
+            ModelTransformationMode transformationMode
     ) {
-        if (stack.isOf(ItemsRegistry.END_TRIDENT) && useInventoryModel) return this.bakedModelManager.getModel(TRIDENT_ID);
+        if (stack.isOf(ItemsRegistry.END_TRIDENT) && shouldUseInventoryModel(transformationMode)) return this.bakedModelManager.getModel(TRIDENT_ID);
         return original;
     }
 }
